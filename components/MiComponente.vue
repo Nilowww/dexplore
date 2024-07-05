3ca65bffae76e96cf6a701acb4494042acfd021d
Martin Alvarez <martinezequielalvarez13@gmail.com>
2024-07-05 17:39:13 -0300

    5th part (Refactor)

--- components/MiComponente.vue
+++ components/MiComponente.vue
@@ -1,38 +1,49 @@
 <template>
-    <p>{{ type }}</p>
-    <input v-model="valor1"/> {{ valor1 }} <br>
-    <input v-model="valor2"/> {{ valor2 }} <br>
-    <p>{{ resultado }}</p>
-    <hr> 
+  <p>{{ type }}</p>
+  <input v-model="valor1" /> {{ valor1 }} <br />
+  <input v-model="valor2" /> {{ valor2 }} <br />
+  <p>{{ resultado }}</p>
+  <hr />
 </template>
 
 <script setup lang="ts">
-    const props = withDefaults(defineProps<{
-        max?: number
-        type: string
-    }>(), {
-        max: 100,
-        type: 'add'
-    });
+const props = withDefaults(
+  defineProps<{
+    max?: number;
+    type: string;
+  }>(),
+  {
+    max: 100,
+    type: "add",
+  }
+);
 
-    const valor1 = ref(Math.round(Math.random() * 100))
-    const valor2 = ref(Math.round(Math.random() * 100))
-    
-    const resultado = computed (() => {
-        let result = 0;
-        if (props.type == 'add') {
-            result = parseInt(valor1.value.toString()) + parseInt(valor2.value.toString());
-        } else if (props.type == 'multiply') {
-            result = parseInt(valor1.value.toString()) * parseInt(valor2.value.toString());
-        } else if (props.type == 'substraction') {
-            result = parseInt(valor1.value.toString()) - parseInt(valor2.value.toString());
-        } else if (props.type == 'division') {
-            result = parseInt(valor1.value.toString()) / parseInt(valor2.value.toString());
-        }
-        if (result > props.max) {
-            return 'Error';
-        }
-        return result;
-    })
-    console.log(resultado)
-</script> 
+const valor1 = ref(Math.round(Math.random() * 100));
+const valor2 = ref(Math.round(Math.random() * 100));
+
+const resultado = computed(() => {
+  let result = 0;
+  switch (props.type) {
+    case "add":
+      result =
+        parseInt(valor1.value.toString()) + parseInt(valor2.value.toString());
+      break;
+    case "multiply":
+      result =
+        parseInt(valor1.value.toString()) * parseInt(valor2.value.toString());
+      break;
+    case "substraction":
+      result =
+        parseInt(valor1.value.toString()) - parseInt(valor2.value.toString());
+      break;
+    case "division":
+      result =
+        parseInt(valor1.value.toString()) / parseInt(valor2.value.toString());
+      break;
+  }
+  if (result > props.max) {
+    return "Error";
+  }
+  return result;
+});
+</script>
