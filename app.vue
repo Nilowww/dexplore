e97eb7148a5dc16b5d9e184c0a83c6384a42d6d8
Lucas Alvarez <lucasalvarez556@gmail.com>
2024-07-05 16:28:59 -0300

    First part (Computeds)

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input placeholder="Nombre" v-model="nombre"/> <br />
+    <input placeholder="Apellido" v-model="apellido"/> <br />
+    <p id="hola">{{ nombreCompleto }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const nombre = ref('');
+const apellido = ref('');
+
+const nombreCompleto = computed(() => {
+  return `${nombre.value} ${apellido.value}`;
+})
+
+</script>
